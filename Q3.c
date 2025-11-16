a58fd2303154026890d666025f96bdc4caee2baf
MuhammadMoosaNomani1 <K250530@nu.edu.pk>
2025-11-16 23:28:34 +0500

    Add files via upload

---
+++ Q3.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main(){ 
+	char names[5][20]={{"Ayesha"},{"Ali"},{"Osman"},{"Roman"},{"Saif"}}; 
+
+	char searchName[20];
+	printf("Enter the Name to Search: ");
+	scanf("%s", searchName);
+	int found=0; 
+	for(int i=0;i<5;i++){
+		if(strcmp(searchName,names[i])==0){
+			found=1;
+		}
+	}
+	if(!found) printf("\nName Not Found!");
+	else printf("\nName Found!");
+	return 0;
+}
