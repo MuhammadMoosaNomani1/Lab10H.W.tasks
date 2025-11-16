49adae1564f66e2d3c76e57441754f8f47e3c6c9
MuhammadMoosaNomani1 <K250530@nu.edu.pk>
2025-11-16 23:06:42 +0500

    Add files via upload

---
+++ Q2.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+#include<stdio.h>
+int main(){
+	char names[5][20];
+	int index;
+	int max=-1;
+	printf("Enter the 5 Names: \n");
+	for(int i=0;i<5;i++){
+		printf("\nName: %d", i+1);
+		scanf("%29s",names[i]);
+	}
+	int len[20];
+		for(int i=0;i<5;i++){
+	len[i]=strlen(names[i]);
+	if(len[i]>max) {
+		max=len[i];
+		index=i;
+	}
+			}	
+			printf("\nLongest Name is %s and its length is %d", names[index],max);
+	return 0;
+}
